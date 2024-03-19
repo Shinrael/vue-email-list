d75f3c0b2097ed85e5f340d326fbb4740ccd275c
Shinrael <mircoelena89@gmail.com>
2024-03-19 16:18:44 +0100

    [add] bonus

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ generare 10 indirizzi email e stamparli in pagina all’interno di una lista.
 ### Svolgimento
 1. Nell'HTML creo un un v-for dentro ad li per creare il mio ciclo di email 
 2. In js creo la funzione per generare la mail tramite axios e nel mounted richiamo la stessa funzione e la ciclo anche qui per riempire l'array che di base è vuoto.
+3. BONUS: creo una const con valore booleana di base false chiamata allEmail e aggiungo un if alla funzione emailGenerate dove una volta arrivato a 10 email nell'array, la costante diventa true. Dopodichè nell'HTML aggiungo un v-if ad UL dove gli dico di procedere solo quando allEmail è uguale a true.
--- js/script.js
+++ js/script.js
@@ -5,15 +5,23 @@ createApp({
   data(){
     return{
       emailArray: [],
+      allEmail : false,
     }
   },
 
   methods:{
     emailGenerate(email){
+      // Chiamata API per generare un indirizzo email casuale
       axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
       .then((response) => {
+        // Estrai l'indirizzo email dalla risposta
         const email = response.data.response;
+        // Aggiungi l'indirizzo email all'array emailArray
         this.emailArray.push(email);
+        // Se sono state generate 10 email, imposta il flag allEmail a true
+        if (this.emailArray.length === 10) {
+          this.allEmail = true;
+        }
       })
     }
   },
@@ -23,6 +31,7 @@ createApp({
   },
 
   mounted(){
+    // Genera 10 indirizzi email chiamando il metodo emailGenerate()
     for (let i = 0; i < 10; i++) {
       this.emailGenerate();
     }
